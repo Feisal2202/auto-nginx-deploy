3d92a79607fb806e61ad1872010aa55cd42f6b67
Posei <55609449+poseisharp@users.noreply.github.com>
2022-01-27 16:01:20 +0700

    Update alkalin.sh

--- alkalin.sh
+++ alkalin.sh
@@ -1,70 +1,61 @@
 echo "1. Dari Github"
 echo "2. Tidak Dari Github"
 read -p "mau pilih yang mana ?: " OPSI
-if opsi == 1
-read -p "masukan domain: " DOMAIN
-read -p "masukan link github: " LINK
+if [ $OPSI == 1 ]
 then
-git clone $LINK /var/www/$DOMAIN/html
-echo $DOMAIN
-chown -R $USER:$USER /var/www/$DOMAIN/html
-cd /var/www/$DOMAIN/html
-echo "<html>
+    read -p "masukan domain: " DOMAIN
+    read -p "masukan link github: " LINK
+    git clone $LINK /var/www/$DOMAIN/
+    echo $DOMAIN
+    chown -R $USER:$USER /var/www/$DOMAIN/
+    cd /etc/nginx/sites-available/
+    echo "
+    server {
+            listen 80;
+            listen [::]:80;
+            root /var/www/$DOMAIN/;  
+            server_name $DOMAIN www.$DOMAIN;
+
+            location / {
+                root /var/www/$DOMAIN/;
+                index index.html index.htm;
+        }
+    }
+    " > $DOMAIN
+    ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled
+    systemctl restart nginx
+if [ $OPSI == 2 ]
+then
+    read -p "masukan domain: " DOMAIN
+    echo $DOMAIN
+    mkdir /var/ww/$DOMAIN/
+    chown -R $USER:$USER /var/www/$DOMAIN/
+    cd /var/www/$DOMAIN/
+    echo "<html>
     <head>
         <title>Welcome to $DOMAIN</title>
     </head>
     <body>
         <h1>Success! The $DOMAIN server block is working!</h1>
     </body>
-</html>" > index.html
-cd /etc/nginx/sites-available/
-echo "
-server {
+    </html>" > index.html
+    cd /etc/nginx/sites-available/
+    echo "server {
         listen 80;
         listen [::]:80;
-        root /var/www/$DOMAIN/html;
-
+        
+        root /var/www/$DOMAIN/;
         server_name $DOMAIN www.$DOMAIN;
+        
         location / {
-        root /var/www/$DOMAIN/html;
-        index index.html index.htm;
-
+            root /var/www/$DOMAIN/;
+            index index.html index.htm;
         }
-}
-" > $DOMAIN
-ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled
-systemctl restart nginx
-if opsi == 2
-read -p "masukan domain: " DOMAIN
-then
-echo $DOMAIN
-chown -R $USER:$USER /var/www/$DOMAIN/html
-cd /var/www/$DOMAIN/html
-echo "<html>
-    <head>
-        <title>Welcome to $DOMAIN</title>
-    </head>
-    <body>
-        <h1>Success! The $DOMAIN server block is working!</h1>
-    </body>
-</html>" > index.html
-cd /etc/nginx/sites-available/
-echo "server {
-        listen 80;
-        listen [::]:80;
-        root /var/www/$DOMAIN/html;
+    }
+    " > $DOMAIN
 
-        server_name $DOMAIN www.$DOMAIN;
-        location / {
-        root /var/www/$DOMAIN/html;
-        index index.html index.htm;
-
-        }
-}
-" > $DOMAIN
-
-ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled
-systemctl restart nginx
+    ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled
+    systemctl restart nginx
 else
-echo "pilihan tidak valid"
+    echo "pilihan tidak valid"
 fi
